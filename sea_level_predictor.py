5e6a522bbeb50a1d4c91d98f84e8809845dbc91b
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2022-02-16 13:08:55 -0500

    Linear regresion fuction added

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,16 +4,23 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv("epa-sea-level.csv")
 
     # Create scatter plot
 
-
+    X = df["Year"]
+    y = df["CSIRO Adjusted Sea Level"]
+    plt.scatter(X,y)
+    plt.show()
+    
+    
     # Create first line of best fit
+    
+    ln_model_1 = linregress(X, y)
 
 
     # Create second line of best fit
-
+    y_2050 = range(df["Year"].min(),2051)
 
     # Add labels and title
 
