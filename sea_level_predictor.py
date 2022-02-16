f7a4d0f00b11563d929564870115922bf2260541
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2022-02-16 16:20:09 -0500

    Project solved everything is OK, all test passed

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
@@ -11,6 +10,7 @@ def draw_plot():
 
     X = df["Year"]
     y = df["CSIRO Adjusted Sea Level"]
+  
     plt.scatter(X,y)
  
     
@@ -18,34 +18,31 @@ def draw_plot():
     # Create first line of best fit
   
     ln_reg_1 = linregress(X, y)
-    X_2050 = np.array(range(df["Year"].min(),2051))
-    y_hat_1 = ln_reg_1.intercept + ln_reg_1.slope * X_2050
+    X_1 = pd.Series([i for i in range(1880,2051)])
+    y_hat_1 = ln_reg_1.intercept + ln_reg_1.slope * X_1
 
-    plt.scatter(X,y)
-    plt.plot(X_2050, y_hat_1)
+    plt.plot(X_1, y_hat_1)
 
 
     # Create second line of best fit
-    df_XX = df[df["Year"] >= 2000]
+    df_2000= df[df["Year"] >= 2000]
 
-    X_1 = df_XX["Year"]
-    y_1 = df_XX["CSIRO Adjusted Sea Level"]
+    X_2 = df_2000["Year"]
+    y_2 = df_2000["CSIRO Adjusted Sea Level"]
     
-    ln_reg_2 = linregress(X_1, y_1)
-    X_2_2050 = np.array(range(df_XX["Year"].min(),2051))
-    y_hat_1 = ln_reg_2.intercept + ln_reg_2.slope * X_2_2050
+    ln_reg_2 = linregress(X_2, y_2)
+    X_3  = pd.Series([i for i in range(2000,2051)])
     
-    plt.scatter(X_1,y_1)
-    plt.plot(X_2_2050, y_hat_1)
+    y_hat_2 = ln_reg_2.intercept + ln_reg_2.slope * X_3
+    
+    plt.plot(X_3, y_hat_2)
   
     
 
     # Add labels and title
-    plt.title("'Rise in Sea Level'")
-    plt.xlabel("'Year'")
-    plt.ylabel("Sea Level (inches)")
-
-    plt.show()
+    plt.title('Rise in Sea Level')
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
     
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
